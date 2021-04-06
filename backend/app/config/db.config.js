1b33b1520e9b89edf035e9b9f5137958d1867332
Maysa Orazova <[maysaorash@gmail.com]>
2021-04-05 22:36:31 -0400

    todo-list

---
+++ backend/app/config/db.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  url: 'mongodb+srv://elifzer:Zerrin2180@cluster0.x9mlw.mongodb.net/todolist?retryWrites=true&w=majority',
+};       
+// module.exports = {
+//   url: 'mongodb+srv://maysaorash:LRQ9NZLXfyXB36UV@cluster0.nmkzg.mongodb.net/toDoLists?retryWrites=true&w=majority',
+// };
